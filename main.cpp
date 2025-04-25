069dbb7fc43c1dc7c1c985f2f34caa2241c839b9
Maicon Leles <mpml@aluno.ifnmg.edu.br>
2025-04-25 18:04:41 -0300

    feat: make game work and print board

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include "state.hpp"
+#include <cstdlib>
 
 int main() {
+
+	State s;
+	unsigned currentPlayer = 0;
+	unsigned pieceSize, x, y;
+	while (s.get_status() == 0) {
+		system("clear");
+		s.print_board();
+		cout << endl << "Player " << currentPlayer << ":" << endl;
+		cin >> pieceSize >> x >> y;
+		s.put_piece(currentPlayer, pieceSize, x, y);
+		currentPlayer = currentPlayer == 0 ? 1 : 0;
+	}
+
+	s.print_board();
+	cout << "Player " << currentPlayer + 1 << "victory!!" << endl;
 }
--- state.hpp
+++ state.hpp
@@ -1,19 +1,22 @@
 #include <vector>
+#include <iostream>
+
+using namespace std;
 
 class State {
 
 public:
 
 	State() {
-		board.resize(3, vector<pair<unsigned, unsigned>(3, pair<unsigned, unsigned>(0, 0)));
-		playerPieces.resize(2, { 0, 2, 2, 2 });
+		board.resize(3, vector<pair<unsigned, unsigned>>(3, pair<unsigned, unsigned>(0, 0)));
+		playersPieces.resize(2, { 0, 2, 2, 2 });
 		status = 0;
 	}
 
 	void put_piece(const unsigned player, const unsigned pieceSize, const unsigned x, const unsigned y) {
-		if (!playerPieces[player][pieceSize].first || board[x][y].second >= pieceSize) return;
+		if (!playersPieces[player][pieceSize] || board[x][y].second >= pieceSize) return;
 
-		--playerPieces[player][pieceSize];
+		--playersPieces[player][pieceSize];
 		board[x][y] = pair<unsigned, unsigned>(player, pieceSize);
 
 		if (board[x][0].first == player && board[x][1].first == player && board[x][2].first == player
@@ -22,6 +25,55 @@ public:
 			|| board[0][2].first == player && board[1][1].first == player && board[2][0].first == player) {
 			status = player;
 		}
+	}
+
+	void print_board() {
+
+		for (unsigned i = 0; i < 3; ++i) {
+			cout << " ";
+			for (unsigned j = 0; j < 3; ++j) {
+				if (board[i][j].first == 0) {
+					switch (board[i][j].second) {
+						case 1:
+							cout << ".";
+							break;
+						case 2:
+							cout << "o";
+							break;
+						case 3:
+							cout << "O";
+							break;
+						default:
+							cout << " ";
+							break;
+					}
+				}
+				else {
+					switch (board[i][j].second) {
+					case 1:
+						cout << "*";
+						break;
+					case 2:
+						cout << "x";
+						break;
+					case 3:
+						cout << "X";
+						break;
+					default:
+						cout << " ";
+						break;
+					}
+				}
+				if(j != 2) cout << " | ";
+			}
+			
+			if(i != 2) cout << endl << "-----------" << endl;
+		}
+		cout << endl;
+	}
+
+	unsigned get_status() {
+		return status;
 	}
 
 private:
